1379662e38d1a3d684dbab0718dcaa3ddaafe442
Erwin Gaitan <erwingaitano@gmail.com>
2021-04-08 22:37:49 -0500

    changed font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,2 @@
-import 'typeface-montserrat';
-import 'typeface-merriweather';
-
 // custom CSS styles
 import './src/index.css';
-
-// Highlighting for code blocks
-import 'prismjs/themes/prism-twilight.css';
